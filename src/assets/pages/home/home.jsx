79f2362a5508ca67c5d28725384ee5a38a07876f
Dionysus <158821418+Cybeles-Eos@users.noreply.github.com>
2025-05-17 19:45:17 +0800

    Update padding

--- src/assets/pages/home/home.jsx
+++ src/assets/pages/home/home.jsx
@@ -4,7 +4,7 @@ import vide from '/Us.mp4'
 import cyber from '../../images/cyber.gif'
 
 function Home(){
-   const right_i = <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="var(--white)" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>;
+   const right_i = <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="var(--white)" viewBox="0 0 16 16"><path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>;
 
    return(
       <>
