c6ff1d2a9c26357428635892f25459a88bebd88f
Dionysus <158821418+Cybeles-Eos@users.noreply.github.com>
2025-05-20 22:32:46 +0800

    Add image

--- src/assets/pages/home/home.jsx
+++ src/assets/pages/home/home.jsx
@@ -1,5 +1,9 @@
 import CSS from './home.module.css'
 import obj_cir from '/done.png'
+
+import out_img from '../../images/out.png'
+import in_img from '../../images/in.png'
+
 import { useEffect, useRef } from "react";
 function Home(){
    const right_i = <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="var(--white)" viewBox="0 0 16 16"><path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/></svg>;
@@ -9,8 +13,8 @@ function Home(){
       <> 
          <article className={CSS.intro_con}>
             <div className={CSS.intro_body}>
-               <h1 className={CSS.title}>Creating Modern
-               Digital Interface</h1>
+               <h1 className={CSS.title}>Creating modern
+               Digital interface</h1>
                <p className={CSS.title_label}>👋 Hi, I’m Zach a creative frontend developer. I design and build clean, responsive, and user-friendly websites that bring ideas to life.</p>
                <div className={CSS.intBtn_con}>
                   <button className={`${CSS.gt_project} ${CSS.intro_btns}`}><span>{right_i}</span>Go to Projects </button>
@@ -18,7 +22,8 @@ function Home(){
                </div>
             </div>
             <div className={CSS.intro_img_con}>
-               <img className={CSS.intro_img} src={obj_cir} alt="intro_img" />
+               <img className={CSS.intro_img_in} src={in_img} alt="intro_img" />
+               <img className={CSS.intro_img_out} src={in_img} alt="intro_img" />
             </div>
          </article>
 
